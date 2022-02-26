835fa0da36455c06925bc3ea8fb0a57590e15e02
ferrari212 <felipe.ferrari.212@gmail.com>
2022-02-26 19:48:13 +0100

    Improving the checker method

--- source/jsm/ship/WaveMotion.js
+++ source/jsm/ship/WaveMotion.js
@@ -1,3 +1,5 @@
+import StateModule from "./StateModule.js";
+
 export default class WaveMotion extends StateModule {
 
 	constructor( ship, states, wavCre, position = 0, critDampPercentage = 20, g = 9.81, rho = 1025 ) {
--- tests/supComparisonFunc.js
+++ tests/supComparisonFunc.js
@@ -1,13 +1,54 @@
 const _ = window._;
 
-export function addRows( className, mode1, mode2, id ) {
+export function addRows( className, mode1, mode2, id, showModes = false ) {
 
-	const check = _.differenceWith( mode1, mode2, _.isEqual );
-	const result = check.length ? `<td style="background-color: #fd7d7d">Not Equal</td>` : `<td style="background-color: #9fe5ab">Equal</td>`;
+	// const check = _.differenceWith( [ mode1 ], [ mode2 ], _.isEqual );
+	const check = _.isEqual( mode1, mode2 );
+	const result = check ? `<td style="background-color: #9fe5ab">Equal</td>` : `<td style="background-color: #fd7d7d">Not Equal</td>`;
 
 	document.getElementById( id ).innerHTML += `<tr>
-    <td>${className}</td>
+    ${addClassName( className )}
+    ${showModes ? addModeValue( mode1, mode2 ) : ""}
     ${result}
   </tr>`;
+
+	console.log( mode1 );
+	console.log( mode2 );
+	console.log( "Difference: ", diff( mode2, mode1 ) );
+
+}
+
+const diff = function ( obj1, obj2 ) {
+
+	// Source:
+	//  https://stackoverflow.com/questions/31683075/how-to-do-a-deep-comparison-between-2-objects-with-lodash
+
+	return _.reduce( obj1, function ( result, value, key ) {
+
+		if ( _.isPlainObject( value ) ) {
+
+			result[ key ] = diff( value, obj2[ key ] );
+
+		} else if ( ! _.isEqual( value, obj2[ key ] ) ) {
+
+			result[ key ] = value;
+
+		}
+
+		return result;
+
+	}, {} );
+
+};
+
+function addClassName( n ) {
+
+	return `<td>${n}</td>`;
+
+}
+
+function addModeValue( m1, m2 ) {
+
+	return `<td>${m1}</td><td>${m2}</td>`;
 
 }
