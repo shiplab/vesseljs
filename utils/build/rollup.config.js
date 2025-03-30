621afdd1b8a7d8db101320dd2711f9dd9eebc83a
ferrari212 <felipe.ferrari.212@gmail.com>
2025-03-30 08:17:57 +0200

    Changing website to the new portfolio version

--- utils/build/rollup.config.js
+++ utils/build/rollup.config.js
@@ -1,27 +1,43 @@
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
+import zip from 'rollup-plugin-zip'
 // import resolve from "@rollup/plugin-node-resolve";
 
-const builds = [ {
-	input: "source/jsm/vessel.js",
-	output: {
-		format: "esm",
-		file: "build/vessel.module.js",
+const zipPlugin = zip({ 
+	file: "build/vessel.zip",
+	// include: ["build/vessel.module.js", "build/vessel.module.min.js"] // Files to include
+});
+
+const builds = [
+	{
+		input: "source/jsm/vessel.js",
+		output: {
+			format: "esm",
+			file: "build/vessel.module.js",
+		},
+		plugins: [
+			commonjs()
+		]
 	},
-	plugins: [
-		commonjs()
-	]
-},
-{
-	input: "source/jsm/vessel.js",
-	output: {
-		format: "esm",
-		file: "build/vessel.module.min.js",
+	{
+		input: "source/jsm/vessel.js",
+		output: {
+			format: "esm",
+			file: "build/vessel.module.min.js",
+		},
+		plugins: [
+			terser()
+		]
 	},
-	plugins: [
-		terser()
-	]
-}
+	{
+		input: ["build/vessel.module.js", "build/vessel.module.min.js"],
+		output: {
+			dir: "./", // required, but irrelevant for zip
+		},
+		plugins: [
+			zipPlugin
+		]
+	}
 ];
 
 export default ( args ) => builds;
